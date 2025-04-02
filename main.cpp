0fad0c048013fa4b735118d751b4a6044f690856
vbharu-96 <117460860+vbharu-96@users.noreply.github.com>
2025-04-02 22:17:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ std::optional<int> pattern1(std::vector<int> seq){
   auto diff = seq[1]-seq[0];
   auto index = 1;
   auto res = std::all_of(seq.begin()+2,seq.end(),[&index,seq,diff](int &n){
-     return n == (seq[index++]+diff);
+     return (n - seq[index++]) == diff; //b-a = constant
   });
   if(res) return(seq[seq.size()-1]+1);  
   //NOTE::[error: operands to '?:' have different types '__gnu_cxx::__alloc_traits<std::allocator<int>, int>::value_type' {aka 'int'} and 'const std::nullopt_t']
