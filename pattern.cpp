cfb5e2dac0417d90dce5db83ffe8d8721fef2767
vbharu-96 <117460860+vbharu-96@users.noreply.github.com>
2025-05-15 11:41:55 +0200

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -5,20 +5,26 @@
 auto addDiff(auto first, auto last){
   return last-first;
 }
+auto add(auto val, auto diff){
+  return val+diff;
+}
+auto mul(auto val, auto diff){
+  return val*diff;
+}
 auto mulDiff(auto first, auto last){
   return last/first;
 }
 //c = a+b constant difference
 std::optional<int> pattern1(std::vector<int> seq){
   // pattern matching 
-  for(auto method:{ addDiff, mulDiff) {
-    auto diff = method(seq[1],seq[0]);
+  for(auto [clue,symbol]:{ {addDiff,add}, {mulDiff,mul}) {
+    auto diff = clue(seq[1],seq[0]);
     auto index = 1;
     auto res = std::all_of(seq.begin()+2,seq.end(),[&index,seq,diff](int &n){
-       return method(n,seq[index++]) == diff; //b-a = constant
+       return clue(n,seq[index++]) == diff; //b-a = constant
     });
     if(res) {
-      return (seq[seq.size()-1]);  
+      return symbol(seq[seq.size()-1], diff);  
     }
   }
   //NOTE::[error: operands to '?:' have different types '__gnu_cxx::__alloc_traits<std::allocator<int>, int>::value_type' {aka 'int'} and 'const std::nullopt_t']
