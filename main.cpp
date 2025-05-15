c363584ad37f92347f8591e1c963fed42ffa6fcd
vbharu-96 <117460860+vbharu-96@users.noreply.github.com>
2025-05-15 11:36:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,6 @@
 #include<vector>
 #include<algorithm>
 
-//c = a+b constant difference
-std::optional<int> pattern1(std::vector<int> seq){
-  // pattern matching 
-  auto diff = seq[1]-seq[0];
-  auto index = 1;
-  auto res = std::all_of(seq.begin()+2,seq.end(),[&index,seq,diff](int &n){
-     return (n - seq[index++]) == diff; //b-a = constant
-  });
-  if(res) return(seq[seq.size()-1]+1);  
-  //NOTE::[error: operands to '?:' have different types '__gnu_cxx::__alloc_traits<std::allocator<int>, int>::value_type' {aka 'int'} and 'const std::nullopt_t']
-  return std::nullopt;
-}
-
-
 int main() {
   std::string input;
   std::vector<int> sequence;
