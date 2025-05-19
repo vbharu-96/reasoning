e5e8cf4ebe1afe71f2b0539e0c9d8153efc5b3f7
vbharu-96 <cbharu96@gmail.com>
2025-05-19 10:43:55 +0200

    clean up

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
 #include<iostream>
 #include<string>
 #include<vector>
-#include<algorithm>
+#include <pattern.h>
+#include<sstream>
 
 int main() {
   std::string input;
   std::vector<int> sequence;
-  std::cout<<"Enter Sequence : "<<std::endl;
-  while(std::getline(std::cin, input, ' ')){
-    sequence.emplace_back(std::stoi(input));
+  for(auto chance =0;chance<3;chance++) {
+      std::cout<<"Enter Sequence : "<<std::endl;
+      std::getline(std::cin, input);
+      std::stringstream ss (input);
+      while(std::getline(ss, input, ' ')){
+        sequence.emplace_back(std::stoi(input));
+      }
+      std::cout<<"Sequence : ";
+      for(auto number: sequence){
+        std::cout<<number<<" ";
+      }
+      std::cout<<"--"<<std::endl;
+      if(sequence.size() < 3) {
+        std::cout<<"WARNING : need to provide atleast 3 numbers. Please provide remining "<<(3-sequence.size())<<" numbers"<<std::endl;
+        continue;
+      }
+      break;
   }
-  std::cout<<"Sequence : ";
-  std::for_each(auto number: sequence){
-    std::cout<<number<<" ";
-  }
-  std::cout<<"--"<<std::endl;
+  
   auto res = pattern1(sequence);
   if(res){
-    std::cout<< "Answer : " <<res<<std::endl;
+    std::cout<< "Answer : " <<*res<<std::endl;
   }
   else{
     std::cout<<"I hope you are not kidding...otherwise, I don't know the answer.... :( "<<std::endl;
--- pattern.cpp
+++ pattern.cpp
@@ -1,32 +1,7 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
-auto addDiff(auto first, auto last){
-  return last-first;
-}
-auto add(auto val, auto diff){
-  return val+diff;
-}
-auto mul(auto val, auto diff){
-  return val*diff;
-}
-auto mulDiff(auto first, auto last){
-  return last/first;
-}
+#include<pattern.h>
 //c = a+b constant difference
 std::optional<int> pattern1(std::vector<int> seq){
-  // pattern matching 
-  for(auto [clue,symbol]:{ {addDiff,add}, {mulDiff,mul}) {
-    auto diff = clue(seq[1],seq[0]);
-    auto index = 1;
-    auto res = std::all_of(seq.begin()+2,seq.end(),[&index,seq,diff](int &n){
-       return clue(n,seq[index++]) == diff; //b-a = constant
-    });
-    if(res) {
-      return symbol(seq[seq.size()-1], diff);  
-    }
-  }
+
   //NOTE::[error: operands to '?:' have different types '__gnu_cxx::__alloc_traits<std::allocator<int>, int>::value_type' {aka 'int'} and 'const std::nullopt_t']
   return std::nullopt;
 }
